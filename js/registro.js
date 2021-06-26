87a2fc21973422d9294c5477b53cfbde871bc281
Gabriela Navarro <gabiluu.n@gmail.com>
2021-06-26 19:35:15 -0300

    agrego funcion de enviar mail

--- js/registro.js
+++ js/registro.js
@@ -27,13 +27,18 @@ const actualizar = function(e){
 }
 const envioForm=function(e){
     e.preventDefault();
-    if(usuario.nombre && usuario.email && usuario.contrasenia){
+  let validar= validarUsuario(usuario.email)
+  if (validar){
+    alert('Ya existe un usuario registrado con ese email');
+    return
+  }else if(usuario.nombre && usuario.email && usuario.contrasenia){
         usuarios.push(usuario)
         localStorage.setItem('usuarios', JSON.stringify(usuarios))
         formulario.reset();
+        enviarConfirmacion(usuario.nombre,usuario.email);
         alert('Registro de usuario finalizado exitosamente') //!Cambiar por alert fachero
         location.replace('/html/home.html')
-
+        
         
     }
 }
@@ -60,5 +65,30 @@ function revisarSesion() {
     if (conectado.email === "adminbiblioteca@gmail.com") {
       document.querySelector(".adminDiv").style.visibility = "visible";
     }
+  }
+}
+
+function enviarConfirmacion(nombre,email){
+  let parametros = {
+    receptor: nombre,
+    emailUsuario: email
+};
+ 
+emailjs.send('service_zqyq8pe', 'registroTemplate', parametros)
+    .then(function(response) {
+       console.log('Registro exitoso', response.status, response.text);
+    }, function(error) {
+       console.log('Algo salió mal', error);
+    });
+}
+
+function validarUsuario(email){
+  let validar = usuarios.find(function(usuario){
+return usuario.email.toLowerCase() === email.toLowerCase()
+  })
+  if (validar){
+      return true;
+  }else{
+      return false;
   }
 }
