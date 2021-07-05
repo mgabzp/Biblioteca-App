7e1ea74b9f558329bfd79ad99ba2952fd8ba78c6
Gabriela Navarro <gabiluu.n@gmail.com>
2021-07-05 20:18:15 -0300

    Version1.0

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ function cargarCards(categoria)
                   <ul class="libro-likes-home">
                     <li><i></i>${elemento.votos}</li>
                     <li><i class="fas fa-thumbs-up fa-1x" style="outline:none; cursor:pointer;" onclick="votar(${idLibro})"></i></li>
-                    <li><i class="fas fa-info fa-1x" style="outline:none; cursor:pointer;" onclick="votar(${idLibro})"></i></li>
+                    <li><i class="fas fa-info fa-1x" style="outline:none; cursor:pointer;" onclick="infoLibro(${idLibro})"></i></li>
                   </ul>
                 </div>
               </div>
@@ -97,33 +97,122 @@ function idGenerator(){
 
 
 
+function infoLibro(id){
+  libro=libros[id]
+
+  document.querySelector("#idLibro").innerText = libro.id;
+  document.querySelector("#titulo_modal").innerText = libro.titulo;
+  document.querySelector("#tituloLibro").innerText = libro.titulo;
+  document.querySelector("#autorLibro").innerText =libro.autor;
+  document.querySelector("#votosLibro").innerText = libro.votos;
+  document.querySelector("#imgLibro").src = libro.imagen;
+  document.querySelector("#anioLibro").innerText = libro.anio;
+  document.querySelector("#editLibro").innerText = libro.editorial;
+  document.querySelector("#descLibro").innerText = libro.contraportada;
+  document.querySelector("#categLibro").innerText = libro.categoria;
+  document.querySelector("#alquilarLib").onclick = function (){ alquilar(libro);}
+  document.querySelector("#modifBtn").onclick = function (){ irModif(libro);}
+  document.querySelector("#elimBtn").onclick = function (){ borrarLibro(id);}
+  $("#modalLibro").modal("show");
+ 
+ 
+}
+function irModif(libro){
+  $("#modalLibro").modal("hide");
+  document.querySelector("#modifId").innerText = libro.id;
+  document.querySelector("#tituloModif").value = libro.titulo;
+  
+  document.querySelector("#autorModif").value =libro.autor;
+  
+  document.querySelector("#imagenModif").value = libro.imagen;
+  document.querySelector("#anioModif").value = libro.anio;
+  document.querySelector("#editorialModif").value = libro.editorial;
+  document.querySelector("#descripcionModif").value = libro.contraportada;
+  document.querySelector("#categoriaModif").value = libro.contraportada;
+  $("#modalModif").modal("show");
+}
+
+function actualizarLibro(e){
+  e.preventDefault(); 
+
+  
+  let indice = libros.findIndex(function (elem) {
+    return elem.id === libro.id;
+  });
+console.log(indice)//! para ver si trae bien el indice
+  
+  libros.splice(indice, 1, libro);
+
+  localStorage.setItem("libros", JSON.stringify(libros));
+  $("#modalModif").modal("hide");
+  cargarTabla(libros);
+ location.reload();
+  
+
+}
+
+
+function modifLibro(e) {
+  
+
+  libro = {
+    ...libro,
+    [e.target.name]: e.target.value,
+  };
+
+  
+}
+
+function borrarLibro(id) {
+  
+
+  let validar=confirm(`¿Estás seguro de que quieres eliminar ${libro.titulo}?`)
+      if (validar) {
+    libros.splice(id, 1);
+    localStorage.setItem("libros", JSON.stringify(libros));
+    
+    swal(
+      "Eliminado!",
+      `Se eliminó el libro ${libro.titulo}`,
+      "success"
+    );
+
+    $("#modalLibro").modal("hide");
+    limpiarTabla();
+    cargarTabla(libros);
+  }
+}
 
 /*Revisar sesion*/
-
-const cerrarSesion = function () {
-  localStorage.removeItem('conectado');
-  location.replace("/html/home.html");
-};
 
 function revisarSesion() {
   document.querySelector("#perfilBtn").style.visibility = "hidden";
   document.querySelector(".soloAdmin").style.visibility = "hidden"
+  document.querySelector(".soloUsuario").style.visibility = "hidden"
+  document.querySelector("#modifBtn").style.visibility = "hidden"
+  document.querySelector("#elimBtn").style.visibility = "hidden"
+  document.querySelector("#alquilarLib").style.visibility = "hidden"
 
   if (conectado) {
+
           let nombre=conectado.nombre+" "
           let espacio = nombre.indexOf(" ");
            
           nombre =nombre.slice(0,espacio);
           
           
-      
-  
+          document.querySelector("#alquilarLib").style.visibility = "visible"
+          document.querySelector(".soloUsuario").style.visibility = "visible"
     document.querySelector("#sesBoton").style.visibility = "hidden";
     document.querySelector("#perfilBtn").style.visibility = "visible";
     document.querySelector("#perfilBtn").innerText =nombre;
     if (conectado.email === "adminbiblioteca@gmail.com") {
       document.querySelector(".soloAdmin").style.visibility = "visible"
       document.querySelector(".soloUsuario").style.visibility = "hidden"
+      document.querySelector("#modifBtn").style.visibility = "visible"
+      document.querySelector("#elimBtn").style.visibility = "visible"
+      document.querySelector("#alquilarLib").style.visibility = "hidden"
+
     }
   } else {
     setTimeout(function () {
@@ -135,9 +224,6 @@ function revisarSesion() {
   }, 8000);
 }
 }
-
-
-
 
 
 
